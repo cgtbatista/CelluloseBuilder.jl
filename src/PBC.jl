07991b59123aed87d3e2f5e1654c0da676e79311
cgtbatista <c203748@dac.unicamp.br>
2024-07-19 17:21:36 -0300

    "Refactored `transformingPBC` function to accept `nsize` instead of `xsize` and `ysize`, added new logic for `style` parameter, and updated VMD script generation."

--- src/PBC.jl
+++ src/PBC.jl
@@ -93,47 +93,19 @@ function transformingPBC(nfrag::Int64, xsize::Int64, ysize::Int64; phase="Iβ", 
 
 end
 
-function transformingPBC(nfrag::Int64, xsize::Int64, ysize::Int64, style::String; phase="Iβ", pbc=nothing, xyzfile="file.xyz", vmd="vmd")
+function transformingPBC(nfrag::Int64, nsize::Int64, style::String; phase="Iβ", pbc=nothing, xyzfile="file.xyz", vmd="vmd")
     
     xyz = joinpath(tempdir(), "cellulose.xyz") ## the new and last XYZ file!
 
-    a=nfrag; boundary=1; n_forbbiden=0; upper=nfrag-1;
+    a=nfrag; boundary=1; n_forbbiden=0; upper=nfrag-1; yea=0;
     forbbiden=Int64[]; remainder=Int64[];
 
-    if phase == "Ib" || phase == "Iβ" || phase == "II"
+    if style == "center"
 
-        if pbc == :all || pbc == :ALL || pbc == :All
-            for b in collect(boundary:1:ysize)
-                n_forbbiden = (2*xsize-1)*b - 1
-                push!(forbbiden, convert(Int64, n_forbbiden))
-            end
-            for b in collect(boundary:1:xsize)
-                a = a - 1
-                push!(forbbiden, convert(Int64, a))
-            end
-        elseif pbc == :a || pbc == :A
-            for b in collect(boundary:1:ysize)
-                n_forbbiden = (2*xsize-1)*b - 1
-                push!(forbbiden, convert(Int64, n_forbbiden))
-            end
-        elseif pbc == :b || pbc == :B
-            for b in collect(boundary:1:xsize)
-                a = a - 1
-                push!(forbbiden, convert(Int64, a))
-            end
-        end
-       
-        for num in 0:upper
-            dummy_logical = 1
-            for ith_forbs in forbbiden
-                if num == ith_forbs
-                    dummy_logical = 0
-                    break
-                end
-            end
-            if dummy_logical == 1
-                remainder = push!(remainder, convert(Int64, num))
-            end
+        for b in collect(boundary:1:nsize)
+            if b > nsize-1; continue; end
+            yea = 2*(b-1)+1
+            push!(remainder, convert(Int64, yea))
         end
 
         sel_fragments = join(remainder, " "); n_fragments = length(remainder);
@@ -147,18 +119,59 @@ function transformingPBC(nfrag::Int64, xsize::Int64, ysize::Int64, style::String
         Base.close(vmdinput)
         vmdoutput = split(Base.read(`$vmd -dispdev text -e $vmdinput_file`, String), "\n")
 
-    elseif (phase == "Ia" || phase == "Iα") && (style == "fibril")
+    elseif style == "origin"
 
-        mv(xyzfile, xyz, force=true)
+        for b in collect(boundary:1:nsize)
+            yea = 2*(b-1)
+            push!(remainder, convert(Int64, yea))
+        end
+
+        sel_fragments = join(remainder, " "); n_fragments = length(remainder);
+        vmdinput_file = tempname() * ".tcl"
+        
+        vmdinput = open(vmdinput_file, "w")
+        Base.write(vmdinput, "mol new \"$xyzfile\" \n")
+        Base.write(vmdinput, "set sel [ atomselect top \"fragment $sel_fragments\" ] \n")
+        Base.write(vmdinput, "\$sel writexyz \"$xyz\" \n")
+        Base.write(vmdinput, "exit \n")
+        Base.close(vmdinput)
+        vmdoutput = split(Base.read(`$vmd -dispdev text -e $vmdinput_file`, String), "\n") 
+ 
+    elseif style == "fibril"
+
+        remainder = "3 4 5 6 7 8 9 14 15 16 17 18 19 20 21 22 23 24 26 27 28 29 30 31 32 33 34 35 36 41 42 43 44 45 46 47"
+        sel_fragments = join(remainder, " "); n_fragments = length(remainder);
+        vmdinput_file = tempname() * ".tcl"
+        
+        vmdinput = open(vmdinput_file, "w")
+        Base.write(vmdinput, "mol new \"$xyzfile\" \n")
+        Base.write(vmdinput, "set sel [ atomselect top \"fragment $sel_fragments\" ] \n")
+        Base.write(vmdinput, "\$sel writexyz \"$xyz\" \n")
+        Base.write(vmdinput, "exit \n")
+        Base.close(vmdinput)
+        vmdoutput = split(Base.read(`$vmd -dispdev text -e $vmdinput_file`, String), "\n")
+
+    elseif style == "monolayer"
+
+        for b in collect(boundary:1:nsize)
+            yea = b*(nsize-1)
+            push!(remainder, convert(Int64, yea))
+        end
+
+        sel_fragments = join(remainder, " "); n_fragments = length(remainder);
+        vmdinput_file = tempname() * ".tcl"
+        
+        vmdinput = open(vmdinput_file, "w")
+        Base.write(vmdinput, "mol new \"$xyzfile\" \n")
+        Base.write(vmdinput, "set sel [ atomselect top \"fragment $sel_fragments\" ] \n")
+        Base.write(vmdinput, "\$sel writexyz \"$xyz\" \n")
+        Base.write(vmdinput, "exit \n")
+        Base.close(vmdinput)
+        vmdoutput = split(Base.read(`$vmd -dispdev text -e $vmdinput_file`, String), "\n")
 
     elseif phase == "III" || phase == "III_I" || phase == "III_i" || phase == "IIIi"
         
         mv(xyzfile, xyz, force=true)
-
-    elseif style == "fibril"
-    elseif style == "origin"
-    elseif style == "monolayer"
-    elseif style == "center"
 
     else
         error("The phase $phase is not implemented yet.")
