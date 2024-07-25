01ece03c181a6f11684ca9389774a6d02b1b7f6c
cgtbatista <c203748@dac.unicamp.br>
2024-07-25 12:38:08 -0300

    "Refactored `transformingPBC` function to use new `_PBC_conditional_` functions, removed duplicated code, and updated function signatures."

--- src/PBC.jl
+++ src/PBC.jl
@@ -1,7 +1,92 @@
+function _PBC_conditional_center(xyz::String, boundary::Int64, remainder::Vector{Int64}; xyzfile="filename.xyz", vmd="vmd", nsize=1)
+
+    for b in collect(boundary:1:nsize)
+        if b > nsize-1; continue; end
+        n = 2*(b-1)+1
+        push!(remainder, convert(Int64, n))
+    end
+
+    sel_fragments = join(remainder, " "); n_fragments = length(remainder);
+    vmdinput_file = tempname() * ".tcl"
+    
+    vmdinput = open(vmdinput_file, "w")
+    Base.write(vmdinput, "mol new \"$xyzfile\" \n")
+    Base.write(vmdinput, "set sel [ atomselect top \"fragment $sel_fragments\" ] \n")
+    Base.write(vmdinput, "\$sel writexyz \"$xyz\" \n")
+    Base.write(vmdinput, "exit \n")
+    Base.close(vmdinput)
+    vmdoutput = split(Base.read(`$vmd -dispdev text -e $vmdinput_file`, String), "\n")
+
+    return xyz, sel_fragments, n_fragments, vmdoutput
+end
+
+function _PBC_conditional_origin(xyz::String, boundary::Int64, remainder::Vector{Int64}; xyzfile="filename.xyz", vmd="vmd", nsize=1)
+
+    for b in collect(boundary:1:nsize)
+        n = 2*(b-1)
+        push!(remainder, convert(Int64, n))
+    end
+
+    sel_fragments = join(remainder, " "); n_fragments = length(remainder);
+    vmdinput_file = tempname() * ".tcl"
+    
+    vmdinput = open(vmdinput_file, "w")
+    Base.write(vmdinput, "mol new \"$xyzfile\" \n")
+    Base.write(vmdinput, "set sel [ atomselect top \"fragment $sel_fragments\" ] \n")
+    Base.write(vmdinput, "\$sel writexyz \"$xyz\" \n")
+    Base.write(vmdinput, "exit \n")
+    Base.close(vmdinput)
+    vmdoutput = split(Base.read(`$vmd -dispdev text -e $vmdinput_file`, String), "\n")
+
+    return xyz, sel_fragments, n_fragments, vmdoutput
+end
+
+function _PBC_conditional_monolayer(xyz::String, boundary::Int64, remainder::Vector{Int64}; xyzfile="filename.xyz", vmd="vmd", nsize=1)
+
+    for b in collect(boundary:1:nsize)
+        n = b*(nsize-1)
+        push!(remainder, convert(Int64, n))
+    end
+
+    sel_fragments = join(remainder, " "); n_fragments = length(remainder);
+    vmdinput_file = tempname() * ".tcl"
+    
+    vmdinput = open(vmdinput_file, "w")
+    Base.write(vmdinput, "mol new \"$xyzfile\" \n")
+    Base.write(vmdinput, "set sel [ atomselect top \"fragment $sel_fragments\" ] \n")
+    Base.write(vmdinput, "\$sel writexyz \"$xyz\" \n")
+    Base.write(vmdinput, "exit \n")
+    Base.close(vmdinput)
+    vmdoutput = split(Base.read(`$vmd -dispdev text -e $vmdinput_file`, String), "\n")
+
+    return xyz, sel_fragments, n_fragments, vmdoutput
+end
+
+function _PBC_conditional_fibril(xyz::String; xyzfile="filename.xyz", vmd="vmd", fibril=nothing)
+
+    if isnothing(fibril)
+        remainder = "3 4 5 6 7 8 9 14 15 16 17 18 19 20 21 22 23 24 26 27 28 29 30 31 32 33 34 35 36 41 42 43 44 45 46 47"
+    end
+
+    sel_fragments = join(remainder, " "); n_fragments = length(remainder);
+    vmdinput_file = tempname() * ".tcl"
+    
+    vmdinput = open(vmdinput_file, "w")
+    Base.write(vmdinput, "mol new \"$xyzfile\" \n")
+    Base.write(vmdinput, "set sel [ atomselect top \"fragment $sel_fragments\" ] \n")
+    Base.write(vmdinput, "\$sel writexyz \"$xyz\" \n")
+    Base.write(vmdinput, "exit \n")
+    Base.close(vmdinput)
+    vmdoutput = split(Base.read(`$vmd -dispdev text -e $vmdinput_file`, String), "\n")
+
+    return xyz, sel_fragments, n_fragments, vmdoutput
+end
+
+
 """
 
     transformingPBC(nfrag::Int64, xsize::Int64, ysize::Int64; phase="Iβ", pbc=nothing, xyzfile="file.xyz", vmd="vmd")
-    transformingPBC(nfrag::Int64, xsize::Int64, ysize::Int64, style::String; phase="Iβ", pbc=nothing, xyzfile="file.xyz", vmd="vmd")
+    transformingPBC(style::String, nsize::Int64; phase="Iβ", fibril=nothing, xyzfile="file.xyz", vmd="vmd")
 
 This function applies the periodic boundary conditions on the cellulose fibrils over the axis `a`, `b`, and `both`.
 After this transformation, you export the cellulose fibril XYZ file with the cartesian coordinates.
@@ -93,85 +178,37 @@ function transformingPBC(nfrag::Int64, xsize::Int64, ysize::Int64; phase="Iβ", 
 
 end
 
-function transformingPBC(nfrag::Int64, nsize::Int64, style::String; phase="Iβ", pbc=nothing, xyzfile="file.xyz", vmd="vmd")
+function transformingPBC(style::String, nsize::Int64; phase="Iβ", fibril=nothing, xyzfile="file.xyz", vmd="vmd")
     
     xyz = joinpath(tempdir(), "cellulose.xyz") ## the new and last XYZ file!
 
-    a=nfrag; boundary=1; n_forbbiden=0; upper=nfrag-1; yea=0;
-    forbbiden=Int64[]; remainder=Int64[];
+    boundary=1; remainder=Int64[];
+    
+    if phase == "Ib" || phase == "Iβ" || phase == "II"
 
-    if style == "center"
-
-        for b in collect(boundary:1:nsize)
-            if b > nsize-1; continue; end
-            yea = 2*(b-1)+1
-            push!(remainder, convert(Int64, yea))
+        if style == "center"
+            xyz, sel_fragments, n_fragments, vmdoutput = _PBC_conditional_center(xyz, boundary, remainder, xyzfile=xyzfile, vmd=vmd, nsize=nsize)
+        elseif sytle == "origin"
+            xyz, sel_fragments, n_fragments, vmdoutput = _PBC_conditional_origin(xyz, boundary, remainder, xyzfile=xyzfile, vmd=vmd, nsize=nsize)
+        elseif style == "fibril"
+            xyz, sel_fragments, n_fragments, vmdoutput = _PBC_conditional_fibril(xyz, xyzfile=xyzfile, vmd=vmd, fibril=fibril)
+        else
+            error("The phase $phase does not supports $style, only the styles center, origin and fibril.")
         end
 
-        sel_fragments = join(remainder, " "); n_fragments = length(remainder);
-        vmdinput_file = tempname() * ".tcl"
-        
-        vmdinput = open(vmdinput_file, "w")
-        Base.write(vmdinput, "mol new \"$xyzfile\" \n")
-        Base.write(vmdinput, "set sel [ atomselect top \"fragment $sel_fragments\" ] \n")
-        Base.write(vmdinput, "\$sel writexyz \"$xyz\" \n")
-        Base.write(vmdinput, "exit \n")
-        Base.close(vmdinput)
-        vmdoutput = split(Base.read(`$vmd -dispdev text -e $vmdinput_file`, String), "\n")
+    elseif phase == "Ia" || phase == "Iα"
 
-    elseif style == "origin"
-
-        for b in collect(boundary:1:nsize)
-            yea = 2*(b-1)
-            push!(remainder, convert(Int64, yea))
+        if style == "monolayer"
+            xyz, sel_fragments, n_fragments, vmdoutput = _PBC_conditional_monolayer(xyz, boundary, remainder, xyzfile=xyzfile, vmd=vmd, nsize=nsize)
+        elseif style == "fibril"
+            xyz, sel_fragments, n_fragments, vmdoutput = _PBC_conditional_fibril(xyz, xyzfile=xyzfile, vmd=vmd, fibril=fibril)
+        else
+            error("The phase $phase does not supports $style, only the styles monolayer and fibril.")
         end
-
-        sel_fragments = join(remainder, " "); n_fragments = length(remainder);
-        vmdinput_file = tempname() * ".tcl"
-        
-        vmdinput = open(vmdinput_file, "w")
-        Base.write(vmdinput, "mol new \"$xyzfile\" \n")
-        Base.write(vmdinput, "set sel [ atomselect top \"fragment $sel_fragments\" ] \n")
-        Base.write(vmdinput, "\$sel writexyz \"$xyz\" \n")
-        Base.write(vmdinput, "exit \n")
-        Base.close(vmdinput)
-        vmdoutput = split(Base.read(`$vmd -dispdev text -e $vmdinput_file`, String), "\n") 
- 
-    elseif style == "fibril"
-
-        remainder = "3 4 5 6 7 8 9 14 15 16 17 18 19 20 21 22 23 24 26 27 28 29 30 31 32 33 34 35 36 41 42 43 44 45 46 47"
-        sel_fragments = join(remainder, " "); n_fragments = length(remainder);
-        vmdinput_file = tempname() * ".tcl"
-        
-        vmdinput = open(vmdinput_file, "w")
-        Base.write(vmdinput, "mol new \"$xyzfile\" \n")
-        Base.write(vmdinput, "set sel [ atomselect top \"fragment $sel_fragments\" ] \n")
-        Base.write(vmdinput, "\$sel writexyz \"$xyz\" \n")
-        Base.write(vmdinput, "exit \n")
-        Base.close(vmdinput)
-        vmdoutput = split(Base.read(`$vmd -dispdev text -e $vmdinput_file`, String), "\n")
-
-    elseif style == "monolayer"
-
-        for b in collect(boundary:1:nsize)
-            yea = b*(nsize-1)
-            push!(remainder, convert(Int64, yea))
-        end
-
-        sel_fragments = join(remainder, " "); n_fragments = length(remainder);
-        vmdinput_file = tempname() * ".tcl"
-        
-        vmdinput = open(vmdinput_file, "w")
-        Base.write(vmdinput, "mol new \"$xyzfile\" \n")
-        Base.write(vmdinput, "set sel [ atomselect top \"fragment $sel_fragments\" ] \n")
-        Base.write(vmdinput, "\$sel writexyz \"$xyz\" \n")
-        Base.write(vmdinput, "exit \n")
-        Base.close(vmdinput)
-        vmdoutput = split(Base.read(`$vmd -dispdev text -e $vmdinput_file`, String), "\n")
 
     elseif phase == "III" || phase == "III_I" || phase == "III_i" || phase == "IIIi"
         
-        mv(xyzfile, xyz, force=true)
+        error("The phase $phase does not supports $style or any other style.")
 
     else
         error("The phase $phase is not implemented yet.")
